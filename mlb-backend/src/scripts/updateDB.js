2435e04959f313124de89c43fb7025be31183d97
Mateo Maturana <mateomaturana@yahoo.com>
2021-08-11 21:26:32 -0400

    added warnings

--- mlb-backend/src/scripts/updateDB.js
+++ mlb-backend/src/scripts/updateDB.js
@@ -1,3 +1,6 @@
 import { populateProjDBToday } from '../simulation/simulate';
 
+/* Not running this script in the morning has some weird results
+ * due to potential ongoing games */
+
 populateProjDBToday();
--- mlb-backend/src/simulation/simulate.js
+++ mlb-backend/src/simulation/simulate.js
@@ -55,76 +55,6 @@ async function runAll() {
 }
 
 async function loadDummyData() {
-  // dummy data as of 8/8
-  await TeamResult.deleteMany({});
-  const divisions = ['ALE', 'ALC', 'ALW', 'NLE', 'NLC', 'NLW'];
-  for (const divisionCur of divisions) {
-    const yankees = new TeamResult({
-        code: "NYY",
-        division: divisionCur,
-        currentWins: 61,
-        currentLosses: 49,
-        projWins: 88.0,
-        projLosses: 74.0,
-        playoffOdds: 27.2,
-        divisionOdds: 1.7,
-        WCOdds: 25.5,
-        pennantOdds: 2.0,
-        championshipOdds: 0.6});
-    yankees.save();
-    const rays = new TeamResult({
-      code: "TB",
-      division: divisionCur,
-      currentWins: 67,
-      currentLosses: 44,
-      projWins: 97.2,
-      projLosses: 64.8,
-      playoffOdds: 96.4,
-      divisionOdds: 79.3,
-      WCOdds: 17.1,
-      pennantOdds: 29.9,
-      championshipOdds: 17.2});
-    rays.save();
-    const redsox = new TeamResult({
-      code: "BOS",
-      division: divisionCur,
-      currentWins: 65,
-      currentLosses: 48,
-      projWins: 91.3,
-      projLosses: 70.7,
-      playoffOdds: 61.9,
-      divisionOdds: 11.4,
-      WCOdds: 50.5,
-      pennantOdds: 7.1,
-      championshipOdds: 2.9});
-    redsox.save();
-    const jays = new TeamResult({
-      code: "TOR",
-      division: divisionCur,
-      currentWins: 59,
-      currentLosses: 50,
-      projWins: 90.6,
-      projLosses: 71.4,
-      playoffOdds: 54.9,
-      divisionOdds: 7.6,
-      WCOdds: 47.3,
-      pennantOdds: 10.1,
-      championshipOdds: 5.8});
-    jays.save();
-    const orioles = new TeamResult({
-      code: "BAL",
-      division: divisionCur,
-      currentWins: 38,
-      currentLosses: 71,
-      projWins: 58.9,
-      projLosses: 103.1,
-      playoffOdds: 0,
-      divisionOdds: 0,
-      WCOdds: 0,
-      pennantOdds: 0,
-      championshipOdds: 0});
-    orioles.save();
-  }
   await DateResult.deleteMany({});
   const august8 = new DateResult({
     date: '2021-08-08',
@@ -687,7 +617,7 @@ function getProbability(team1, team2, pitcher1Rating, pitcher2Rating) {
   // TEMP (WIP)
   const team1Rating = (pitcher1Rating + team1.pitcherRating + team1.hitterRating) / 3;
   const team2Rating = (pitcher2Rating + team2.pitcherRating + team2.hitterRating) / 3;
-  return (team1Rating - 400) / ((team2Rating - 400) + (team1Rating - 400));
+  return (team1Rating - 450) / ((team2Rating - 450) + (team1Rating - 450));
 }
 
 export async function simulateOneDate(id, date) {
