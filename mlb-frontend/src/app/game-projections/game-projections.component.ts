9a6ea093681774e1414e2db5c624fca5e49517df
Mateo Maturana <mateomaturana@yahoo.com>
2021-08-14 23:17:36 -0400

    small change to rockies color

--- mlb-frontend/src/app/game-projections/game-projections.component.ts
+++ mlb-frontend/src/app/game-projections/game-projections.component.ts
@@ -43,7 +43,7 @@ export class GameProjectionsComponent implements OnInit {
     this.teamColors.set('Chicago White Sox', 'rgba(196, 206, 212, .8)');
     this.teamColors.set('Cincinnati Reds', 'rgba(198, 1, 31, .8)');
     this.teamColors.set('Cleveland Indians', 'rgba(12, 35, 64, .8)');
-    this.teamColors.set('Colorado Rockies', 'rgba(51, 0, 111, .8)');
+    this.teamColors.set('Colorado Rockies', 'rgba(51, 0, 111, .7)');
     this.teamColors.set('Detroit Tigers', 'rgba(12, 35, 60, .8)');
     this.teamColors.set('Houston Astros', 'rgba(235, 110, 31, .8)');
     this.teamColors.set('Kansas City Royals', 'rgba(0, 70, 135, .8)');
